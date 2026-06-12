9478e2fa8ef2561f865e041f7fcac279f38b62ff
Raghav Tiwari <ragji345@gmail.com>
2026-06-12 23:28:24 +0530

    Q2

---
+++ birthday.py
@@ -0,0 +1,15 @@
+#BITHDAY CLACULATION
+import datetime
+
+bdate=input("Enter your birth day and month (DD-MM):").split("-")
+bdt=datetime.date.today()
+a=bdt.year
+
+if(int(bdate[1])>=bdt.month):
+    a+=1
+
+bd=datetime.date(a,int(bdate[1]),int(bdate[0]))
+if(bd==bdt):
+    print("🎉Happy Birthday🎉")
+else:
+    print("Your birthday is in",bd-bdt)
