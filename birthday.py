ca79973751a87aec495d62f9906a8966d6741fad
Raghav Tiwari <ragji345@gmail.com>
2026-06-13 11:29:23 +0530

    Q1

--- birthday.py
+++ birthday.py
@@ -5,7 +5,9 @@ bdate=input("Enter your birth day and month (DD-MM):").split("-")
 bdt=datetime.date.today()
 a=bdt.year
 
-if(int(bdate[1])>=bdt.month):
+if(int(bdate[1])==bdt.month and bdt.day > int(bdate[0])):
+    a+=1
+elif(int(bdate[1])<bdt.month):
     a+=1
 
 bd=datetime.date(a,int(bdate[1]),int(bdate[0]))
